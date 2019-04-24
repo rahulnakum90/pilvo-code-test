0cda44804e8aa68c35b062e4c51b80c0b6d1b65a
Rahul Nakum <rnakum@assurecare.com>
2019-04-23 23:30:41 -0400

    health360 medication

--- plivo-test/src/test/java/tests/health360/MedicationsTest.java
+++ plivo-test/src/test/java/tests/health360/MedicationsTest.java
@@ -15,6 +15,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@SuppressWarnings("unchecked")
 public class MedicationsTest extends MedicationsPage {
     SearchMember searchMember;
     @BeforeClass
